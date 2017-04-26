cc4e83e3e5b797587383d7edc7dcc0cb86c6f76d
Yuan Li <lixx2100@umn.edu>
2017-04-26 16:57:51 -0500

    Add 1008, 51nod

---
+++ src/_51nod/_1008.java
@@ -0,0 +1,56 @@
+package _51nod;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _1008 {
+
+    public static void main(String[] args) throws Exception {
+        Reader.init(System.in);
+        BufferedWriter cout = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Reader.nextInt();
+        long p = Reader.nextInt();
+        long prod = 1;
+        for (long i = 1; i <= n; i++)
+            prod = prod * i % p;
+        System.out.println(prod);
+
+        cout.close();
+    }
+
+    /**
+     * Class for buffered reading int and double values
+     */
+    static class Reader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        /**
+         * call this method to initialize reader for InputStream
+         */
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        /**
+         * get next word
+         */
+        static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                // TODO add check for eof if necessary
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
