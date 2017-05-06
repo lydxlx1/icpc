1b4d7b722775f0671caade8db09e0118f2d98c78
Yuan Li <lydxlx@gmail.com>
2017-05-06 00:32:47 -0500

    Add 1242, 51nod

---
+++ src/_51nod/_1242.java
@@ -0,0 +1,81 @@
+package _51nod;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _1242 {
+
+    static int M = 2;
+    static long MOD = 1000000009;
+
+    static long[][] mul(long[][] A, long[][] B) {
+        long[][] C = new long[M][M];
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < M; j++)
+                for (int k = 0; k < M; k++)
+                    C[i][j] = (C[i][j] + A[i][k] * B[k][j] % MOD) % MOD;
+        return C;
+    }
+
+    static long[][] pow(long[][] A, long n) {
+        long[][] res = new long[M][M];
+        for (int i = 0; i < M; i++)
+            res[i][i] = 1;
+        while (n > 0) {
+            if (n % 2 == 1)
+                res = mul(res, A);
+            A = mul(A, A);
+            n /= 2;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader.init(System.in);
+        BufferedWriter cout = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long[][] A = {
+                {1, 1},
+                {1, 0},
+        };
+        long n = Long.parseLong(Reader.next());
+        System.out.println(pow(A, n)[0][1]);
+
+        cout.close();
+    }
+
+    /**
+     * Class for buffered reading int and double values
+     */
+    static class Reader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        /**
+         * call this method to initialize reader for InputStream
+         */
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        /**
+         * get next word
+         */
+        static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                // TODO add check for eof if necessary
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
