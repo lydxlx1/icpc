e8c511de6135202a236474d3ac23f1c2c68e7109
Yuan Li <lixx2100@umn.edu>
2017-05-07 16:58:46 -0500

    Add 1003, 51nod

---
+++ src/_51nod/_1003.java
@@ -0,0 +1,57 @@
+package _51nod;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _1003 {
+
+    public static void main(String[] args) throws Exception {
+        Reader.init(System.in);
+        BufferedWriter cout = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Reader.nextInt();
+        int ans = 0;
+        while (n > 0) {
+            ans += n / 5;
+            n /= 5;
+        }
+        System.out.println(ans);
+
+        cout.close();
+    }
+
+    /**
+     * Class for buffered reading int and double values
+     */
+    static class Reader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        /**
+         * call this method to initialize reader for InputStream
+         */
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        /**
+         * get next word
+         */
+        static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                // TODO add check for eof if necessary
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
