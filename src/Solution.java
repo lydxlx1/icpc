505fd345ef2a58c4716880de74fe55ce60c42d88
Yuan Li <lydxlx@fb.com>
2022-09-05 17:03:06 -0700

    Add Solution.java

---
+++ src/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.util.stream.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Reader.init(System.in);
+        BufferedWriter cout = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        cout.close();
+    }
+
+
+    /**
+     * Class for buffered reading int and double values
+     */
+    static class Reader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        /**
+         * call this method to initialize reader for InputStream
+         */
+        static void init(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+            tokenizer = new StringTokenizer("");
+        }
+
+        /**
+         * get next word
+         */
+        static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                // TODO add check for eof if necessary
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+}
